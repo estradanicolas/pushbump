cc166a39dbba4bd14a650a908eb9675012e405ef
Nicolas Estrada <59927633+estradanicolas@users.noreply.github.com>
2020-05-16 12:04:42 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import './styles.css';
+import SideBar from './components/SideBar.js';
+import Sports from './components/sports.js'
+import Footer from './components/footer.js';
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <div>
+        <SideBar />
+        <App />
+        <Footer />
+    </div>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
